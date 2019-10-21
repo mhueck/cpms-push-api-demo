cdbbf686bfc13cfe3c156e7496dfcacd6551cfb7
Markus Hück <markus.hueck@sap.com>
2019-10-21 17:23:37 +0200

    Revert "fixes"
    This reverts commit 7a7c83902a2519a26de1fb54a5379a6fdfec3470.

--- src/scripts/push-client.js
+++ src/scripts/push-client.js
@@ -122,6 +122,9 @@ class PushClient {
 
       this._stateChangeCb(this._state.SUBSCRIBED);
 
+      // Update the current state with the
+      // subscriptionid and endpoint
+      this._subscriptionUpdate(subscription);
     })
     .catch((err) => {
       console.log('setUpPushPermission() ', err);
